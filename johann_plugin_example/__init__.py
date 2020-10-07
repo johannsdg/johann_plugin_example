57eb61b9d1bcbab3e9e89ae3aeb0a0560dfa0cc6
Jeffrey James <lobotmcj@gmail.com>
2020-10-07 09:50:14 -0400

    bump: <none> to 0.1.0-alpha
    Signed-off-by: Jeffrey James <lobotmcj@gmail.com>

--- johann_plugin_example/__init__.py
+++ johann_plugin_example/__init__.py
@@ -2,7 +2,7 @@
 # Use of this source code is governed by a BSD-3-clause license that can
 # be found in the LICENSE file. See the AUTHORS file for names of contributors.
 
-__version__ = "0.1.0a0"
+__version__ = "0.1.0-alpha"
 
 import johann_plugin_example.tasks
 from johann_plugin_example import config as example_config
